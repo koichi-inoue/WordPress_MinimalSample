2e569f8691045fa8ec8263abf938a6ae6790538f
koichi-inoue <inoue.ko@gmail.com>
2018-08-23 17:14:18 +0900

    Add SideBar

--- index.php
+++ index.php
@@ -70,6 +70,35 @@
 
     </article>
 
+    <aside>
+
+      <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+          <?php dynamic_sidebar( 'sidebar-1' ); ?>
+      <?php else: ?>
+
+      <div class="widget">
+        <h2>Recent posts</h2>
+        <ul>
+          <?php $args = array(
+            'type' => 'postbypost',
+            'limit' => 5
+          );
+          wp_get_archives($args); ?>
+        </ul>
+      </div>
+
+      <div class="widget">
+        <h2>Meta</h2>
+      <ul>
+        <?php wp_register(); ?>
+        <li><?php wp_loginout(); ?></li>
+        <?php wp_meta(); ?>
+      </ul>
+      </div>
+
+    <?php endif; ?>
+    </aside>
+
   </main>
 
   <footer>
